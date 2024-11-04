e69b26d212d44235ded67e7d1f34ccf027e8ccd4
digruber.emese <digruber.emese@28-DIAK5>
2024-11-04 13:28:51 +0100

    Add project files.

---
+++ csopi3_doga/Program.cs
@@ -0,0 +1,33 @@
+﻿using csopi3_doga;
+using System.Text;
+
+List<Versenyzo2> versenyzok = [];
+using StreamReader sr = new("..\\..\\..\\src\\forras.txt", Encoding.UTF8);
+while (!sr.EndOfStream) versenyzok.Add(new(sr.ReadLine()));
+
+var f1 = versenyzok.Where(v => v.Kategoria == "25-29");
+Console.WriteLine($"versenyzok szama a 25-29-es kategoriaban: {f1.Count()}");
+
+var f2 = versenyzok.Average(v=> 2014-v.Szul);
+Console.WriteLine($"Atlag eletkor a verenyzoknel: {f2:0.00} ev");
+
+var f3 = versenyzok.Sum(v => v.VersenyIdok["úszás"].TotalHours);
+Console.WriteLine($"versenyzok uszassal toltott osszes ideje: {f3:0.00} ora");
+
+var f4 = versenyzok.Where(v => v.Kategoria == "elit").Average(v => v.VersenyIdok["úszás"].TotalMinutes);
+Console.WriteLine($"Atlagos uszasi ido elit kategoriaban: {f4:0.00} perc");
+
+var f5 = versenyzok.Where(v => v.Nem).MinBy(v => v.Osszido);
+Console.WriteLine($"A ferfi gyoztes: {f5}");
+
+var f6 = versenyzok.GroupBy(v => v.Kategoria);
+Console.WriteLine($"a versenyt befejezok rajtszama a korkategoriankent:");
+foreach (var grp in f6)
+{
+    Console.WriteLine($"\t{grp.Key}: ({grp.Count()})");
+    foreach (var versenyzo in grp)
+    {
+        Console.WriteLine($"\t\t{versenyzo}");
+    }
+}
+    
---
+++ csopi3_doga/Versenyzo2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csopi3_doga
+{
+    internal class Versenyzo2
+    {
+        public string Nev { get; set; }
+        public int Szul { get; set; }
+        public string RajtSzam { get; set; }
+        public bool Nem { get; set; }
+        public string Kategoria { get; set; }
+        public Dictionary<string, TimeSpan> VersenyIdok { get; set; }
+
+        public override string ToString() => $"[{RajtSzam}] {Nev} ({Kategoria})";
+        public int Osszido => (int)VersenyIdok.Values.Sum(x => x.TotalSeconds);
+
+        public Versenyzo2(string sor)
+        {
+            string[] d = sor.Split(';');
+            Nev = d[0];
+            Szul = int.Parse(d[1]);
+            RajtSzam = d[2];
+            Nem = d[3] == "f";
+            Kategoria = d[4];
+
+            VersenyIdok = new()
+            {
+                {"úszás", TimeSpan.Parse(d[5])},
+                {"I. depó", TimeSpan.Parse(d[6])},
+                {"kerékpározás", TimeSpan.Parse(d[7])},
+                {"II. depó", TimeSpan.Parse(d[8])},
+                {"futás", TimeSpan.Parse(d[9])},
+            };
+        }
+    }
+}
